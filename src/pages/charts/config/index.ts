68954166b1b77ddbcddcae2564e1e4feac0368cb
coder7 <ren760824@163.com>
2022-08-11 18:13:42 +0800

    charts组件修改

---
+++ src/pages/charts/config/index.ts
@@ -0,0 +1,19 @@
+import { ChartsConfigInterface } from '@/components/Charts/types';
+
+export let lineChartsConfig: ChartsConfigInterface = {
+  type: 1,
+  xData: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
+export let linesChartsConfig: ChartsConfigInterface = {
+  type: 1,
+  xData: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
+export let barChartsConfig: ChartsConfigInterface = {
+  type: 2,
+  xData: ['Q1', 'Q2', 'Q3', 'Q4'],
+  barWidth: '16px',
+};
+export let lineBarChartsConfig: ChartsConfigInterface = {
+  type: 4,
+  xData: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
